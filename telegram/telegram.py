559d1dc8c5d92e5c0a6a874928b8fba09d4fec4b
Jordan Raychev <jpraychev@gmail.com>
2022-10-03 05:50:15 +0300

    Bot class refactored

--- telegram/telegram.py
+++ telegram/telegram.py
@@ -1,27 +1,34 @@
-from requests import request
+""" Simplest telegram bot """
+
 import os
 import time
+from typing import Any
+from requests import request
+from .utils import command_parser
 
 class Bot:
+    """ Simplest telegram bot """
 
-    def __init__(self, token: str = None):
+    def __init__(self, token:str=None):
+
+        if token is None and os.getenv('TELEGRAM_BOT_TOKEN') is None:
+            raise ValueError('You have to either provide a token in the init method or define'
+                             'an environmental variable `TELEGRAM_BOT_TOKEN`')
         self.token = os.getenv('TELEGRAM_BOT_TOKEN') if token is None else token
-        
+
         # Prepare all urls that would be used so we don't have to do it in each method
         self.base_url = 'https://api.telegram.org/bot{token}/{method}'
         self.get_updates_url = self.base_url.format(token=self.token, method='getUpdates')
         self.send_message_url = self.base_url.format(token=self.token, method='sendMessage')
-        self.timeout = 10
+        self.__commands = {}
 
-    def __make_request(self, request_method, url, data=None, params=None, timeout=None):
-        
-        if timeout is None:
-            timeout = self.timeout
-        return request(request_method, url, data=data, params=params, timeout=timeout)
-    
-    def __acknowledge_update(self, update_id:int) -> bool:
+    def __make_request(self, request_method, url, **kwargs):
+        return request(request_method, url, timeout=10, **kwargs)
+
+    def __ack_message(self, msg) -> bool:
         """Acknoledges that the previos update has been received. The system works
-        by sending another getUpdates request with incremented update_id
+        by sending last's message update_id and incrementing it by 1. The new value
+        should be supplied to the offeset parameter in the entities array.
 
         Args:
             update_id (int): The update_id of the message we would like to acknowledge
@@ -32,55 +39,127 @@ class Bot:
         Returns:
             bool: True if the previos message was acknoledged correctly
         """
+        update_id = msg['update_id']
 
         payload = {'offset' : update_id + 1}
-        r = self.__make_request('get', self.get_updates_url, data=payload)
+        res = self.__make_request('get', self.get_updates_url, data=payload)
 
-        if r.status_code != 200:
-            raise Exception(f'An error occured with message acknowledgement')
-        return True
-    
-    def send_msg(self, to:str, msg:str) -> None:
-        
-        payload = {'chat_id' : to, 'text' : msg}  
-
-        r = self.__make_request('post', self.send_message_url, data=payload)
-
-        if r.status_code != 200:
-            raise Exception(f'An error occured with message notification - {r.status_code}')
+        if res.status_code != 200:
+            raise Exception('An error occured with message acknowledgement')
         return True
 
-    def __execute_command(self, about):
-        print(f'cmd has been executed for {about}')
-        # TO DO
-        # Implement method that executes a command on the host machine
-        pass
-    
-    def __get_message(self):
-        """Get a single message by polling
+    def __cmd_registered(self, user_cmd:str) -> bool:
+        if self.__commands.get(user_cmd) is None:
+            print(f'The supplied command "{user_cmd}" was not registered. '
+                   'Message will be acknoledged but action not executed!')
+            return False
+        return True
+
+    def __notify_sender(self, chat_id, msg):
+        return self.send_msg(chat_id, msg)
+
+    def __execute_command(self, msg:dict[str, Any]) -> bool:
+        cmd = msg['message']['text']
+        chat_id = msg['message']['chat']['id']
+
+        if self.__cmd_registered(cmd):
+            command = self.__commands.get(cmd)
+            # TO DO
+            # Subprocess call for the particular command
+            self.__notify_sender(chat_id, f'{cmd} executed successfully. Output is down below:')
+            return True
+        self.__notify_sender(chat_id, f'{cmd} was not executed because it is not registered. '
+                             'Please register')
+        return False
+
+    def __get_message(self) -> dict:
+        """Gets a single message from by polling.
+
+        Note: If a message does not have entities array in the results, that might
+        be due to the fact that the sent message is not a registered bot command.
+
+        Returns:
+            dict: Returns the last unacknowledged message
+        """
+        try:
+            res = self.__make_request('get', self.get_updates_url)
+            if res.json()['result']:
+                return res.json()['result'][0]
+        except Exception as err: # Better exception handling is required
+            raise Exception from err
+
+    def register_commands(self, file) -> dict[str, str]:
+        """Registers a set of command for the bot to execute
+
+        commands:
+            1:
+                name: Test command
+                description: Test description
+                command: /test
+                action: Hi jordan
+            2:
+                name: Test command
+                description: Test description
+                command: /test1
+                action: Hi jordan1
+        Args:
+            file (yaml): Yaml like file as described above
+
+        Returns:
+            dict[str, str]: Parsed dictionary
         """
 
-        return 
+        data = command_parser(file)
+        self.__commands = data
+
+    def send_msg(self, to:str, msg:str) -> bool:
+        """A helper method that sends a message to particular telegram chat id.
+
+        Args:
+            to (str): A specific chat id where messages will be sent.
+            msg (str): Message
+
+        Raises:
+            Exception: If response status code is different from 200, we raise an exception
+
+        Returns:
+            bool: Return true if the message was sent successfully
+        """
+
+        payload = {'chat_id' : to, 'text' : msg}
+        res = self.__make_request('post', self.send_message_url, data=payload)
+
+        if res.status_code != 200:
+            raise Exception(f'An error occured with message notification - {res.status_code}')
+        return True
+
     def listen(self, interval:int) -> None:
         """Main loop that polls messages for a particular bot as defined by the
         get_updates_url.
-        
+
         The way Telegram's polling system works is by getting the last messages from
         getUpdates API. Once message is received you have to acknowledgement it,
-        otherwise it will stay in the queue.
-        The acknowledgement system works by sending another request to getUpdates API
-        with incremented update_id parameter. See self.__acknowledge_update method
-        
+        otherwise it will stay in the queue and the poll system will always return all messages
+        until properly acknowledged. By design the polling system returns all messaages
+        in a `results` array and you need to iterate over them one by one in order to read them.
+        To avoid going over all messages at once, the __get_message API return only the first
+        result from the array, guaranteeing that this is the first unacknoledged message from
+        the queue.
+        A message can be acknoledged after sending an updated update_id to getUpdates API.
+        See self.__ack_message method
+
         Args:
             interval (int): Interval between each poll.
         """
         print('Bot started listening for updates...')
+        try:
+            while True:
+                time.sleep(interval)
+                msg = self.__get_message()
 
-        while True:
-            time.sleep(interval)
-            r = self.__make_request('get', self.get_updates_url)
+                if msg:
+                    self.__execute_command(msg)
+                    self.__ack_message(msg)
 
-            if r.json()['result']:
-                update_id = r.json()['result'][0]['update_id']
-                self.__acknowledge_update(update_id)
-                self.__execute_command(about=update_id)
+        except KeyboardInterrupt:
+            print('Bot interrupted. Stopping...')
